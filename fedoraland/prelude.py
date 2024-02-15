c3fd0c2473525a5f3124134b0003ce9c55c10c52
me <rosllarr@tutanota.com>
2024-02-15 11:43:48 +0700

    draft config files

--- fedoraland/prelude.py
+++ fedoraland/prelude.py
@@ -1,6 +1,7 @@
 from fedoraland.software import Software
 from fedoraland.util import (
     done_text,
+    enable_repository,
     install_package,
     is_repository_exists,
     skip_text
@@ -10,12 +11,6 @@ from fedoraland.util import (
 dnf_plugins_core = Software(name="dnf-plugins-core",
                             packages=["dnf-plugins-core"])
 dnf_plugins_core.install()
-
-# Enable RPMFusion
-# free_rpm = "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
-# nonfree_rpm = "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
-# rpmfusion = Software(name="RPMFusion", packages=[free_rpm, nonfree_rpm])
-# rpmfusion.install()
 
 
 def enable_rpmfusion():
@@ -47,6 +42,29 @@ def enable_rpmfusion():
 # Enable RPMFusion
 enable_rpmfusion()
 
+# Enable openh264 library
+if is_repository_exists("fedora-cisco-openh264"):
+    skip_text("fedora-cisco-openh264 is already enabled")
+else:
+    enable_repository("fedora-cisco-openh264")
+    done_text("fedora-cisco-openh264 is enabled")
+
 # Remove podman to prevent conflicts with docker
 podman = Software(name="Podman", packages=["podman"])
-podman.remove()
+podman.install()
+
+# Install essential packages
+openssl = Software(name="openssl", packages=["openssl"])
+openssl.install()
+
+python3_pip = Software(name="python3-pip", packages=["python3-pip"])
+python3_pip.install()
+
+python3_devel = Software(name="python3-devel", packages=["python3-devel"])
+python3_devel.install()
+
+keepassxc = Software(name="keepassxc", packages=["keepassxc"])
+keepassxc.install()
+
+git = Software(name="git", packages=["git"])
+git.install()
